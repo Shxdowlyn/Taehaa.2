cf9a89eaeffd930fc124c10877eb077de0c73f96
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-15 13:10:55 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1210,10 +1210,8 @@ if (!['owner-unbanchat.js'].includes(name) && chat && chat.isBanned && !isROwner
 if (name != 'owner-unbanchat.js' && name != 'owner-exec.js' && name != 'owner-exec2.js' && name != 'tool-delete.js' && chat?.isBanned && !isROwner) return 
 if (m.text && user.banned && !isROwner) {
 if (user.antispam > 2) return
-m.reply(`📵 Usted está baneado en mi sistema y no podrás usar EliteBot .
-
-• Motivo : Toxicidad hacia EliteBot`)
-user.antispam++	
+m.reply(`🚫 *ESTÁ BANEADO(A), NO PUEDE USAR LOS COMANDOS*\n📑 *MOTIVO: ${user.messageSpam === 0 ? 'NO ESPECIFICADO' : user.messageSpam}*\n⚠️ \`\`\`SI ESTE BOT ES CUENTA OFICIAL Y TIENE EVIDENCIA QUE RESPALDE QUE ESTE MENSAJE ES UN ERROR, PUEDE EXPONER SU CASO EN:\`\`\`👉 *${ig}*\n👉 ${asistencia}`)
+user.antispam++
 return
 }
 
