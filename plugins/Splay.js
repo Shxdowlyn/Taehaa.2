4b229e033a4398b26935bb21e5e2958f320da83d
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-15 12:21:13 -0500

    Update Splay.js

--- plugins/Splay.js
+++ plugins/Splay.js
@@ -1,127 +1,57 @@
-/* 
-- aquie de que se trata de CPF By Alex
-- Power By Team Code Titans
-- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S 
-*/
+//Codígo creado por Destroy wa.me/584120346669
 
-// DOWNLOAD - SPOTIFY
+import fs from 'fs';
+import path from 'path';
 
-import axios from 'axios'
-import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix }) => {
+if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+    return m.reply(`${emoji} El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*`);
+    }
+    
+    let who;
+    if (m.mentionedJid.length > 0) {
+        who = m.mentionedJid[0];
+    } else if (m.quoted) {
+        who = m.quoted.sender;
+    } else {
+        who = m.sender;
+    }
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let name = conn.getName(who);
+    let name2 = conn.getName(m.sender);
+    m.react('🥵');
 
-    if (!text) return conn.reply(m.chat, `Por favor proporciona el nombre de una canción o artista.`, m)
+    let str;
+    if (m.mentionedJid.length > 0) {
+        str = `\`${name2}\` *tiene sexo fuertemente con* \`${name || who}\`.`;
+    } else if (m.quoted) {
+        str = `\`${name2}\` *tiene sexo con* \`${name || who}\`.`;
+    } else {
+        str = `\`${name2}\` *tiene sexo apasionadamente.*`.trim();
+    }
+    
+    if (m.isGroup) {
+        let pp = 'https://telegra.ph/file/a2ad1dd463a935d5dfd17.mp4'; 
+        let pp2 = 'https://telegra.ph/file/e3abb2e79cd1ccf709e91.mp4'; 
+        let pp3 = 'https://telegra.ph/file/c5be4a906531c6731cd41.mp4';
+        let pp4 = 'https://telegra.ph/file/9c4b894e034c290df75e4.mp4';
+        let pp5 = 'https://telegra.ph/file/3246f62c61a0ebebcb5c8.mp4';
+        let pp6 = 'https://telegra.ph/file/820460f05d76bb2329bbc.mp4';
+        let pp7 = 'https://telegra.ph/file/2072f260302c6bb97682a.mp4';
+        let pp8 = 'https://telegra.ph/file/22d0ef801c93c1b2ac074.mp4';
+        let pp9 = 'https://telegra.ph/file/6f66fd1974e8df1496768.mp4';
+        
+        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp8, pp9];
+        const video = videos[Math.floor(Math.random() * videos.length)];
 
-    try {
-        let songInfo = await spotifyxv(text)
-        if (!songInfo.length) throw `No se encontró la canción.`
-        let song = songInfo[0]
-        const res = await fetch(`https://archive-ui.tanakadomp.biz.id/download/spotify?url=${song.url}`)
-
-        if (!res.ok) throw `Error al obtener datos de la API, código de estado: ${res.status}`
-
-        const data = await res.json().catch((e) => { 
-            console.error('Error parsing JSON:', e)
-            throw "Error al analizar la respuesta JSON."
-        })
-
-        if (!data || !data.result || !data.result.data || !data.result.data.download) throw "No se pudo obtener el enlace de descarga."
-
-        const info = `「✦」Descargando: ${data.result.data.title}\n\n> 👤 *Artista:* ${data.result.data.artis}\n> 💽 *Álbum:* ${song.album}\n> 🕒 *Duración:* ${timestamp(data.result.data.durasi)}\n> 🔗 *Enlace:* ${song.url}`
-
-        await conn.sendMessage(m.chat, { text: info, contextInfo: { forwardingScore: 9999999, isForwarded: false, 
-        externalAdReply: {
-            showAdAttribution: true,
-            containsAutoReply: true,
-            renderLargerThumbnail: true,
-            title: packname,
-            body: dev,
-            mediaType: 1,
-            thumbnailUrl: data.result.data.image,
-            mediaUrl: data.result.data.download,
-            sourceUrl: data.result.data.download
-        }}}, { quoted: m })
-
-        conn.sendMessage(m.chat, { audio: { url: data.result.data.download }, fileName: `${data.result.data.title}.mp3`, mimetype: 'audio/mp4', ptt: true }, { quoted: m })
-
-    } catch (e1) {
-        m.reply(`${e1.message || e1}`)
+        let mentions = [who];
+        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, mentions }, { quoted: m });
     }
 }
 
-handler.help = ['spotify', 'music']
-handler.tags = ['downloader']
-handler.command = ['spotify', 'splay']
-handler.group = true
-handler.register = true
+handler.help = ['sexo/sex @tag'];
+handler.tags = ['nsfw'];
+handler.command = ['sexo','sex'];
+handler.group = true;
 
 export default handler
-
-async function spotifyxv(query) {
-    let token = await tokens()
-    let response = await axios({
-        method: 'get',
-        url: 'https://api.spotify.com/v1/search?q=' + query + '&type=track',
-        headers: {
-            Authorization: 'Bearer ' + token
-        }
-    })
-    const tracks = response.data.tracks.items
-    const results = tracks.map((track) => ({
-        name: track.name,
-        artista: track.artists.map((artist) => artist.name),
-        album: track.album.name,
-        duracion: timestamp(track.duration_ms),
-        url: track.external_urls.spotify,
-        imagen: track.album.images.length ? track.album.images[0].url : ''
-    }))
-    return results
-}
-
-async function tokens() {
-    const response = await axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: 'Basic ' + Buffer.from('acc6302297e040aeb6e4ac1fbdfd62c3:0e8439a1280a43aba9a5bc0a16f3f009').toString('base64')
-        },
-        data: 'grant_type=client_credentials'
-    })
-    return response.data.access_token
-}
-
-function timestamp(time) {
-    const minutes = Math.floor(time / 60000)
-    const seconds = Math.floor((time % 60000) / 1000)
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
-}
-
-async function getBuffer(url, options) {
-    try {
-        options = options || {}
-        const res = await axios({
-            method: 'get',
-            url,
-            headers: {
-                DNT: 1,
-                'Upgrade-Insecure-Request': 1
-            },
-            ...options,
-            responseType: 'arraybuffer'
-        })
-        return res.data
-    } catch (err) {
-        return err
-    }
-}
-
-async function getTinyURL(text) {
-    try {
-        let response = await axios.get(`https://tinyurl.com/api-create.php?url=${text}`)
-        return response.data
-    } catch (error) {
-        return text
-    }
-}
