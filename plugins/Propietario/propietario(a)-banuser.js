4047d89bab279372015b2e0a87b5ce31d78d5ddb
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-20 15:56:22 -0500

    Update propietario(a)-banuser.js

--- plugins/Propietario/propietario(a)-banuser.js
+++ plugins/Propietario/propietario(a)-banuser.js
@@ -41,7 +41,7 @@ await conn.reply(m.chat, lenguajeGB.smsPropban5(), null, { mentions: [user] })
 await conn.reply(m.chat, lenguajeGB.smsPropban7(usedPrefix, command, number), null, m)
 console.log(e) 
 }}
-handler.customPrefix = /estado|estatus/i 
+handler.customPrefix = /bloquear/i 
 handler.admin = true
 handler.owner = true
 export default handler
