ae147e1fc3c831f6e9e537a34bb73f3ce1e71c22
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-20 10:29:42 -0500

    Update herramientas-chatGPT.js

--- plugins/Herramientas/herramientas-chatGPT.js
+++ plugins/Herramientas/herramientas-chatGPT.js
@@ -49,5 +49,5 @@ let gpt = await fetch(`${apis}/ia/gptweb?text=${text}`)
 let res = await gpt.json()
 await m.reply(res.gpt)
 }}
-handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
+handler.command = /^(openai|chatgpt|ia|ai|bot|chatgpt2|ia2)$/i;
 export default handler;
