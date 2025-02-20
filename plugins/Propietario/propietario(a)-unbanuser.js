d3c818c2a26201e8f8335c11fbf81764561e2a19
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-20 16:02:48 -0500

    Update propietario(a)-unbanuser.js

--- plugins/Propietario/propietario(a)-unbanuser.js
+++ plugins/Propietario/propietario(a)-unbanuser.js
@@ -6,11 +6,12 @@ else who = m.chat
 if (!who) throw `${mg}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝘼𝙇𝙂𝙐𝙄𝙀𝙉 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*\n\n𝙏𝘼𝙂 𝙎𝙊𝙈𝙀𝙊𝙉𝙀 𝙏𝙊 𝙐𝙉𝘽𝘼𝙉\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
 let users = global.db.data.users
 users[who].banned = false
-conn.reply(m.chat, `${eg}𝙀𝙇/𝙇𝘼 𝙐𝙎𝙐𝘼𝙍𝙄𝙊(𝘼) 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊(𝘼) 🤩\n𝙋𝙊𝘿𝙍𝘼 𝙐𝙎𝘼𝙍 𝘼 ${gt}\n\n𝙏𝙃𝙀 𝙐𝙎𝙀𝙍 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 🤠\n𝙐𝙎𝙀 𝐄𝐋𝐈𝐓𝐄 𝐁𝐎𝐓`, m)
+conn.reply(m.chat, `Este pana fue desbloqueado en mi base de datos ☑️.`, m)
 }
 handler.help = ['unbanuser']
 handler.tags = ['owner']
 handler.command = /^desbloquear$/i
 handler.group = true
 handler.admin = true 
+handler.owner = true
 export default handler
