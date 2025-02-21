2402d9e4c32c7ff974fe271ee9f0e7c2abe6b099
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-21 12:12:01 -0500

    Update Dinámicafe.js

--- plugins/Textos romaticos/Dinámicafe.js
+++ plugins/Textos romaticos/Dinámicafe.js
@@ -1,20 +1,3 @@
-let handler = async (m, { conn, usedPrefix, command}) => {
-let pp = ['https://qu.ax/RspuJ.mp4','https://qu.ax/ZHnMG.mp4','https://qu.ax/ZHnMG.mp4','https://qu.ax/RspuJ.mp4']
-
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-if (!who) throw '» *Etiqueta al usuario con el que quieres tomarte un cafesito*\nEjemplo:\n.tomarcafe @kevin'
-let name2 = conn.getName(who)
-let name = conn.getName(m.sender)
-
-await conn.sendMessage(m.chat, { video: { url: pp.getRandom() }, gifPlayback: true, caption: `_*${name}*_` + ' 𝘦𝘴𝘵𝘢 𝘵𝘰𝘮𝘢́𝘯𝘥𝘰𝘴𝘦 𝘶𝘯 𝘤𝘢𝘧𝘦 𝘤𝘰𝘯' + ` _*${name2}*_` + ' ☕\n©𝘌𝘭𝘪𝘵𝘦𝘉𝘰𝘵𝘎𝘭𝘰𝘣𝘢𝘭 -', contextInfo: fakeChannel }, { quoted: m })
-}
-handler.help = ['tomarcafe <@user>']
-handler.tags = ['fun']
-handler.command = ['tomarcafe']
-export default handler
-
 
 
 
