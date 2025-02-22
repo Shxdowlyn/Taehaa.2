e498c6285a304fd45f656cc788b84703dc65075d
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-22 00:24:56 -0500

    Update Simi.js

--- plugins/Simi.js
+++ plugins/Simi.js
@@ -1,37 +1,33 @@
-import translate from '@vitalets/google-translate-api';
-import axios from 'axios';
-import fetch from 'node-fetch';
+import db from '../lib/database.js'
 
-const handler = async (m, {conn, text, command, args, usedPrefix}) => {
+import MessageType from '@whiskeysockets/baileys'
+let impts = 0
+let handler = async (m, { conn, text }) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0]
+    else who = m.chat
+    if (!who) return m.reply('⚠️️ *Taguea al usuario*')
+    let txt = text.replace('@' + who.split`@`[0], '').trim()
+    if (!txt) return m.reply('⚠️️ Ingrese la cantidad de *corazones* que quiere añadir')
+    if (isNaN(txt)) return m.reply('⚠️ *sólo números*')
+    let dmt = parseInt(txt)
+    let corazones = dmt
+    let pjk = Math.ceil(dmt * impts)
+    corazones += pjk
+    if (corazones < 1) return m.reply('⚠️️ Mínimo es  *1*')
+    let users = global.db.data.users
+   users[who].corazones += dmt
 
-if (!text) conn.reply(m.chat, `${emoji} Te faltó el texto para hablar con la *Bot*`, m);
-try {
-// await m.react(emojis)
-const resSimi = await simitalk(text);
-conn.sendMessage(m.chat, { text: resSimi.resultado.simsimi }, { quoted: m });
-} catch {
-throw `${msm} Ocurrió un error.`;
-}};
+    await conn.reply(m.chat, `⊜ *🤍 AÑADIDO*
+┏━━━━━━━━━━━⬣
+┃⋄ *Total:* ${dmt}
+┗━━━━━━━━━━━⬣`, m, rcanal)
+   conn.fakeReply(m.chat, `⊜ *_Recibiste_* \n\n *_+${dmt} corazones 🤍_*`, who, m.text)
+}
 
-handler.help = ['sini', 'bot'];
-handler.tags = ['fun'];
-handler.group = true;
-handler.register = true
-handler.command = ['yuki', 'Yuki']
+handler.help = ['addcorazones *<@user>*']
+handler.tags = ['owner']
+handler.command = ['addcorazones'] 
+handler.rowner = true
 
-export default handler;
-
-async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es") {
-if (!ask) return { status: false, resultado: { msg: "Debes ingresar un texto para hablar con simsimi." }};
-try {
-const response1 = await axios.get(`https://delirius-apiofc.vercel.app/tools/simi?text=${encodeURIComponent(ask)}`);
-const trad1 = await translate(`${response1.data.data.message}`, {to: language, autoCorrect: true});
-if (trad1.text == 'indefinida' || response1 == '' || !response1.data) trad1 = XD // Se usa "XD" para causar error y usar otra opción.  
-return { status: true, resultado: { simsimi: trad1.text }};        
-} catch {
-try {
-const response2 = await axios.get(`https://anbusec.xyz/api/v1/simitalk?apikey=${apikeyyy}&ask=${ask}&lc=${language}`);
-return { status: true, resultado: { simsimi: response2.data.message }};       
-} catch (error2) {
-return { status: false, resultado: { msg: "Todas las API's fallarón. Inténtalo de nuevo más tarde.", error: error2.message }};
-}}}
+export default handler
