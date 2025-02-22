e882618f6276c06068d015c771ea3439fc651357
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-22 00:02:38 -0500

    Update Descargas tiktok.js

--- plugins/Descargas/Descargas tiktok.js
+++ plugins/Descargas/Descargas tiktok.js
@@ -1,75 +1,40 @@
-import fg from 'api-dylux' 
-import axios from 'axios'
-import cheerio from 'cheerio'
-import { tiktok } from "@xct007/frieren-scraper";
-let generateWAMessageFromContent = (await import(global.baileys)).default
-import { tiktokdl } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}${mid.smsTikTok2}\n*${usedPrefix + command} https://vm.tiktok.com/ZMhw4JCuV/*`, fkontak,  m)
-if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}${mid.smsTikTok3}`, fkontak,  m)  
-await conn.reply(m.chat, `✦ ¡Espera por favor!
-Estoy descargando el vídeo sin marca de agua.`, fkontak,  m) 
-try {
-const dataF = await tiktok.v1(args[0])
-conn.sendFile(m.chat, dataF.play, 'tiktok.mp4', `☑️ Video descargado con éxito.
+import fetch from 'node-fetch';
 
-© Powered By Elite Bot`.trim(), m) 
-} catch (e1) {
-try {
-const tTiktok = await tiktokdlF(args[0])
-conn.sendFile(m.chat, tTiktok.video, 'tiktok.mp4', `☑️ Video descargado con éxito.
+var handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) {
+        return conn.reply(m.chat, `${emoji} Por favor, ingresa un enlace de TikTok.`, m);
+    }
 
-© Powered By Elite Bot`.trim(), m) 
-} catch (e2) {
-try {
-let p = await fg.tiktok(args[0]) 
-conn.sendFile(m.chat, p.nowm, 'tiktok.mp4', `☑️ Video descargado con éxito.
+    try {
+        await conn.reply(m.chat, `${emoji} Espere un momento, estoy descargando su video...`, m);
 
-© Powered By Elite Bot`.trim(), m)
-} catch (e3) {
-try { 
-const { author: { nickname }, video, description } = await tiktokdl(args[0])
-const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-conn.sendFile(m.chat, url, 'tiktok.mp4', `☑️ Video descargado con éxito.
+        const tiktokData = await tiktokdl(args[0]);
 
-© Powered By Elite Bot`.trim(), m)
-//handler.limit = 2
-} catch (e4) {
-try{
-const response=await fetch(`https://deliriussapi-oficial.vercel.app/download/tiktok?url=${args[0]}`)
-const dataR = await response.json()
-const { author,title, meta} = dataR.data
-conn.sendFile(m.chat, meta.media[0].org, 'tiktok.mp4', `☑️ Video descargado con éxito.
+        if (!tiktokData || !tiktokData.data || !tiktokData.data.play) {
+            return conn.reply(m.chat, "Error: No se pudo obtener el video.", m);
+        }
 
-© Powered By Elite Bot`.trim(), m) 
+        const videoURL = tiktokData.data.play;
+
+        if (videoURL) {
+            await conn.sendFile(m.chat, videoURL, "tiktok.mp4", `${emoji} Aquí tienes ฅ^•ﻌ•^ฅ`, m);
+        } else {
+            return conn.reply(m.chat, "No se pudo descargar.", m);
+        }
+    } catch (error1) {
+        return conn.reply(m.chat, `Error: ${error1.message}`, m);
+    }
+};
+
+handler.help = ['tiktok'].map((v) => v + ' *<link>*');
+handler.tags = ['descargas'];
+handler.command = ['tiktok', 'tt'];
+handler.group = true;
+
+export default handler;
+
+async function tiktokdl(url) {
+    let tikwm = `https://www.tikwm.com/api/?url=${url}?hd=1`;
+    let response = await (await fetch(tikwm)).json();
+    return response;
 }
-catch (e5){
-try{
-const response=await fetch(`https://deliriusapi-official.vercel.app/download/tiktok?&query=${text}`)
-const dataR = await response.json()
-conn.sendFile(m.chat,dataR.result.link, 'tiktok.mp4', `☑️ Video descargado con éxito.
-
-© Powered By Elite Bot`.trim(), m)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-handler.limit = false
-}}}}}}}
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = /^(tt|tiktok)(dl|nowm)?$/i
-//handler.limit = 2
-export default handler
-
-async function tiktokdlF(url) {
-if (!/tiktok/.test(url)) return 'Enlace incorrecto';
-const gettoken = await axios.get("https://tikdown.org/id");
-const $ = cheerio.load(gettoken.data);
-const token = $("#download-form > input[type=hidden]:nth-child(2)").attr( "value" );
-const param = { url: url, _token: token };
-const { data } = await axios.request("https://tikdown.org/getAjax?", { method: "post", data: new URLSearchParams(Object.entries(param)), headers: { "content-type": "application/x-www-form-urlencoded; charset=UTF-8", "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36" }, });
-var getdata = cheerio.load(data.html);
-if (data.status) {
-return { status: true, thumbnail: getdata("img").attr("src"), video: getdata("div.download-links > div:nth-child(1) > a").attr("href"), audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"), }} else
-return { status: false }}
