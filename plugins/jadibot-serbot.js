adc153260fc18defa7588aacc816f7fe258644d5
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2025-02-22 07:56:49 -0500

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,19 +1,29 @@
-// Código Hecho Por Niño Piña wa.me/50557865603
-let handler = async (m, { conn }) => {
-// React con un emoji al mensaje
-m.react('🌞');
-// Siuuuuuuuuu
-const message = "⏰*DESPERTADOR*⏰ Levántense webones 💪🥵";
-if (m.isGroup) {
-// CrowBot-Ai🎄
-const videoUrl = 'https://files.catbox.moe/xss6jx.mp4'; //  Levantense🗣️🔥🔥
-// Feliz Navidad
-// Deja Los Créditos
-await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption: message }, { mimetype: 'video/mp4' });
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+
+let img = ['https://qu.ax/LEag.jpeg',
+           'https://qu.ax/GaLcn.jpg']
+let texto = `» 𝙈𝘼𝙋𝘼 𝘿𝙀 𝙆𝘼𝙇𝘼𝙃𝘼𝙍𝙄 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ✅`,
+
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
 }
+await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
 }
-handler.help = ['despertar'];
-handler.tags = ['group'];
-handler.command = ['despertar', 'desp', 'despierten', 'levantense'];
-handler.group = true;
-export default handler;
+handler.command = ['cancer'] 
+handler.register = false
+handler.admin = true
+export default handler
