abae18646e360564f706c2132bd1cf701abeb29c
shxdowlynnie <thirdeyebikes@gmail.com>
2026-07-24 09:25:35 -0300

    Actualizar propietario-noetiquetar.js

--- plugins/Propietario/propietario-noetiquetar.js
+++ plugins/Propietario/propietario-noetiquetar.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn, text}) => {
 
  m.reply(`*[ ⚠️ ️]* 𝗡𝗢 𝗘𝗧𝗜𝗤𝗨𝗘𝗧𝗘𝗦 𝗔 𝗠𝗜 𝗖𝗥𝗘𝗔𝗗𝗢𝗥, 𝗖𝗢𝗡𝗧𝗔́𝗖𝗧𝗔𝗧𝗘 𝗖𝗢𝗡 𝗘𝗟 𝗔 𝗦𝗨 𝗣𝗥𝗜𝗩𝗔𝗗𝗢 𝗦𝗜 𝗖𝗥𝗘𝗘𝗦 𝗤𝗨𝗘 𝗔𝗟𝗚𝗢 𝗜𝗠𝗣𝗢𝗥𝗧𝗔𝗡𝗧𝗘.`)
 }
-handler.customPrefix = /@593993370003/i;
+handler.customPrefix = /@5493863447787/i;
 handler.command = new RegExp();
 
 export default handler;
