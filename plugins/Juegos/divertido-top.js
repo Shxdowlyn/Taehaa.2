a05a207cb31f4c26e9e4052eeef5bdb3fa0f3977
shxdowlynnie <thirdeyebikes@gmail.com>
2026-07-24 07:43:58 -0300

    Actualizar divertido-top.js

--- plugins/Juegos/divertido-top.js
+++ plugins/Juegos/divertido-top.js
@@ -27,6 +27,7 @@ function handler(m, {groupMetadata, conn, text}) {
 *9. ${user(i)}*
 *10. ${user(j)}*`;
   m.reply(top, null, {mentions: [a, b, c, d, e, f, g, h, i, j]});
+}
   });
 }
 handler.help = handler.command = ["top"];
