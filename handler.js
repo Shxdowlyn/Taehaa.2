9f1dd1238fa1f0947438484c469e25f7f5f6bb81
shxdowlynnie <thirdeyebikes@gmail.com>
2026-07-25 14:12:06 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1079,28 +1079,16 @@ console.error(e)
 console.log("SENDER:", m.sender)
 console.log("OWNERS:", global.owner)
 
-let sender = conn.decodeJid(m.sender || '')
+const sender = conn.decodeJid(m.sender)
+const senderNumber = sender.replace(/[^0-9]/g, '')
 
-if (sender.endsWith('@lid')) {
-    try {
-        const lidData = await conn.onWhatsApp(sender)
-        if (lidData?.[0]?.jid) sender = lidData[0].jid
-    } catch (e) {}
-}
-
-const senderNumber = String(sender).replace(/[^0-9]/g, '')
-
-const ownerList = global.owner.map(([number]) =>
-    String(number).replace(/[^0-9]/g, '')
-)
+const ownerList = global.owner.map(([number]) => number.replace(/[^0-9]/g, ''))
 
 const isROwner = ownerList.includes(senderNumber)
 
 const isOwner = isROwner || m.fromMe
 
-const isMods = isOwner || global.mods.map(v =>
-    String(v).replace(/[^0-9]/g, '')
-).includes(senderNumber)
+const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '')).includes(senderNumber)
 //const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 const isPrems = isROwner || global.db.data.users[m.sender].premiumTime > 0
 if (opts['queque'] && m.text && !(isMods || isPrems)) {
