83e23813de8da921c3d6e772bba365f61d2206cc
shxdowlynnie <thirdeyebikes@gmail.com>
2026-07-25 13:58:09 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1078,9 +1078,16 @@ console.error(e)
 
 console.log("SENDER:", m.sender)
 console.log("OWNERS:", global.owner)
-const isROwner = [conn.decodeJid(global.conn.user.id), ...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+
+const ownerList = global.owner.map(([number]) => number.replace(/[^0-9]/g, ''))
+
+const senderNumber = m.sender.replace(/[^0-9]/g, '')
+
+const isROwner = ownerList.includes(senderNumber)
+
 const isOwner = isROwner || m.fromMe
-const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+
+const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '')).includes(senderNumber)
 //const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 const isPrems = isROwner || global.db.data.users[m.sender].premiumTime > 0
 if (opts['queque'] && m.text && !(isMods || isPrems)) {
