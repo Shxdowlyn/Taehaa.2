6c03c4b550c450c6ed1f1e2b0c91344a8b31f620
shxdowlynnie <thirdeyebikes@gmail.com>
2026-07-25 14:02:06 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1076,18 +1076,25 @@ jadibotmd: false,
 console.error(e)
 }
 
-console.log("SENDER:", m.sender)
-console.log("OWNERS:", global.owner)
+let sender = m.sender
 
-const ownerList = global.owner.map(([number]) => number.replace(/[^0-9]/g, ''))
+if (sender.endsWith('@lid')) {
+    try {
+        sender = await conn.signalRepository.lidMapping.getPNForLID(sender.split('@')[0])
+    } catch {}
+}
 
-const senderNumber = m.sender.replace(/[^0-9]/g, '')
+const senderNumber = sender.replace(/[^0-9]/g, '')
+
+const ownerList = global.owner.map(([number]) =>
+    number.replace(/[^0-9]/g, '')
+)
 
 const isROwner = ownerList.includes(senderNumber)
-
 const isOwner = isROwner || m.fromMe
-
-const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '')).includes(senderNumber)
+const isMods = isOwner || global.mods.map(v =>
+    v.replace(/[^0-9]/g, '')
+).includes(senderNumber)
 //const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 const isPrems = isROwner || global.db.data.users[m.sender].premiumTime > 0
 if (opts['queque'] && m.text && !(isMods || isPrems)) {
